a128dbc729b74588cea86c9649aaff4c242fed0f
karthikgg <karthik.gottiparthy@gmail.com>
2015-07-08 19:36:44 -0700

    Initial indexpage with projects

--- bobblehead/webapp/urls.py
+++ bobblehead/webapp/urls.py
@@ -5,10 +5,14 @@ from . import views
 urlpatterns = [
     # ex: /projects/
     url(r'^$', views.index, name='index'),
-    # ex: /polls/5/
-    #url(r'^(?P<project_id>[0-9]+)/$', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    #url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    # ex: /polls/5/vote/
-    #url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    # ex: /projects/5/
+    url(r'^(?P<project_id>[0-9]+)/$', views.project_detail, name='detail'),
+    # ex: /projects/createproject
+    url(r'createproject/$',
+        views.create_project, name='createproject'),
+    # ex: /projects/5/updateproject
+    url(r'^(?P<project_id>[0-9]+)/update/$',
+        views.update_project, name='update'),
+    # ex: /projects/5/deleteproject
+    #url(r'^(?P<question_id>[0-9]+)/dele/$', views.vote, name='vote'),
 ]
--- bobblehead/webapp/views.py
+++ bobblehead/webapp/views.py
@@ -3,19 +3,38 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from webapp.models import Project
+from django.http import Http404
 
 def index(request):
     """Description goes here"""
     latest_project_list = Project.objects.order_by('posted')[:10]
+    context = {'latest_project_list': latest_project_list}
+    return render(request, 'webapp/index.html', context)
+
     output = ', '.join([p.title for p in latest_project_list])
     return HttpResponse(output)
 
 def project_detail(request, project_id):
     """Description goes here"""
-    return None
+    try:
+        project = Project.objects.get(pk=project_id)
+    except Project.DoesNotExist:
+        raise Http404 ("Project does not exist")
+    return render (request, 'webapp/details.html', {'project': project})
+
 
 def create_project(request):
     """Description goes here"""
+    return render (request, 'webapp/createproject.html', context)
+
+def update_project(request):
+    """Description goes here"""
+
+    return None
+
+def delete_project(request):
+    """Description goes here"""
+
     return None
 
 def create_user(request):
