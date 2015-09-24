feb10fe4863fd07d9115176947a4c945c3b41d93
raghuramsj@hotmail.com <raghuramsj@gmail.com>
2015-09-23 20:34:35 -0700

    changed static path

--- bobblehead/config/django_secret.py
+++ bobblehead/config/django_secret.py
@@ -1,3 +1,3 @@
 # django secret
 
-DJANGO_SECRET = ''
+DJANGO_SECRET = ')=q%e5l$2(vnh2@ei5p&z^59j7ku=5#m@&f&u@tc=e73ywtfb5'
--- bobblehead/static/bobblehead/js/bobblehead.js
+++ bobblehead/static/bobblehead/js/bobblehead.js
@@ -9,18 +9,22 @@
 
   app.controller('ProjectsController', ['$http', function($http) {
 		var data = this;
-		data.data = [];
 		$http.get('/webapp/projects_JSON/').then(function(response){
 			data.data = JSON.parse(JSON.parse(response.data));
     });
   }]);
 
 // http://www.htmlxprs.com/post/32/creating-an-angularjs-autocomplete-tag-input-widget
-	app.controller('FormController', ['$http', '$window', function($http, $window){
-		// Need to get this.tages from server
-		this.tags = ['python', 'javascript', 'java', 'css', 'django', 'j', 'jello'];
+	app.controller('FormController', ['$scope', '$http', '$window', function($scope, $http, $window){
+		// Set defaults where necessary
+		this.difficulty = 'EASY';
+
+		// Need to get this.tags from server
+		this.tags = ['python', 'javascript', 'java', 'css', 'django', 'jello'];
 		this.tags = this.tags.sort();
-		this.searchText = ''
+
+		// Tags!
+		this.searchText = '';
 		this.suggestions = [];
 		this.selectedTags = [];
 		this.selectedIndex = 0;
@@ -52,40 +56,96 @@
 				}
 			}
 			else if (event.keyCode === 13 || event.keyCode === 9) { //enter pressed
-				event.preventDefault();
-				this.addToSelectedTags(this.selectedIndex);
-				this.searchText = '';
-				this.search();
+				if (this.searchText.length > 0) {
+					event.preventDefault();
+					this.addToSelectedTags(this.selectedIndex);
+					this.searchText = '';
+					this.search();
+				}
 			}
 		}
 
 		this.addToSelectedTags = function(index) {
-			if (this.selectedTags.indexOf(this.suggestions[index]) == -1 && this.searchText !== '') {
-				this.selectedTags.push(this.suggestions[index]);
+			if (this.suggestions.length > 0) {
+				if (this.selectedTags.indexOf(this.suggestions[index]) == -1) {
+					this.selectedTags.push(this.suggestions[index]);
+				}
 			}
-			console.log(this.selectedTags);
+			else {
+				if (this.selectedTags.indexOf(this.searchText) == -1 && this.searchText !== '') {
+					this.selectedTags.push(this.searchText);
+				}
+			}
+			document.getElementById('id_tags').required = false;
 		}
 
 		this.removeTag = function(index) {
 			this.selectedTags.splice(index, 1);
-			console.log(this.selectedTags);
 		}
 
+		// Articles
+		this.article = '';
+		this.articles = [];
+		this.maxArticlesText = '';
+		this.maxArticles = 5;
+
+		this.addArticle = function(event) {
+			if (event.keyCode === 13 || event.keyCode === 9) { //Enter or Tab pressed
+				// To-dp: validate for URL instead of >0
+				if (this.article.length > 0 && this.articles.indexOf(this.article) == -1) {
+					event.preventDefault();
+					if (this.articles.length < this.maxArticles) {
+						this.articles.push(this.article);
+						this.article = '';
+					}
+					if (this.articles.length == this.maxArticles) {
+						this.maxArticlesText = 'Maximum number of articles reached';
+					}
+				}
+			}
+		}
+
+		this.removeArticle = function(index) {
+			this.articles.splice(index, 1);
+			this.maxArticlesText = '';
+		}
+
+
+		// Submit function
 		this.submit = function() {
-			var payload = {
-				'title': this.title,
-				'collaborators': 1,
-				'difficulty': this.difficulty,
-				'description': this.description,
-				'tags_list': this.selectedTags,
+			// Define required fields
+			var required = [this.title, this.difficulty, this.description, this.selectedTags];
 
-			};
-			console.log(payload);
-			$http.post('/webapp/create_project/', payload).
-				then (function(response) {
-					$window.location.href = '/webapp/' + response.data;
-				});
+			// Set counter to check each required field actually contains data
+			var allDataPresent = '0';
+
+			// Check that all the required fields contain data
+			var checkData = function(value) {
+				if (value && value.length > 0) {
+					allDataPresent++;
+				}
+			}
+			required.forEach(checkData);
+
+			// If all required fields contain data, then submit
+			if (allDataPresent == required.length) {
+
+				// Payload for POST request
+				var payload = {
+					'title': this.title,
+					'collaborators': 1,
+					'difficulty': this.difficulty,
+					'description': this.description,
+					'tags_list': this.selectedTags,
+					'articles': this.articles
+				};
+
+				// POST request to submit data
+				$http.post('/webapp/create_project/', payload).
+					then (function(response) {
+						$window.location.href = '/webapp/' + response.data;
+					});
+			}
 		}
-
 	}]);
 })();
